d3a0f557b65c114032151d78c05a1d88505282e8
sadgurukgp <53691693+sadgurukgp@users.noreply.github.com>
2020-02-16 12:59:40 +0530

    updated vpc and subnet

--- var.tf
+++ var.tf
@@ -1,20 +1,20 @@
 variable "aws_vpc" {
     description = "VPC"
-    default = "vpc-6d8f7507"
+    default = "vpc-9265eee8"
 }
 
 variable "aws_subnet" {
     description = "aws subnet"
-    default = "subnet-81b9a0fc"
+    default = "subnet-4b1b5e65"
 }
 
 variable "aws_subnet2" {
     description = "aws subnet2"
-    default = "subnet-81b9a0fc"
+    default = "subnet-1593112b"
 }
 
 variable "aws_key" {
     description = "aws key"
-    default = "DevOpsKey"
+    default = "dec2019"
 }
 
