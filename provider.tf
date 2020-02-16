653339378066a8de118ce23174c4ffcfd3e7a514
sadgurukgp <53691693+sadgurukgp@users.noreply.github.com>
2020-02-16 17:53:41 +0530

    password updated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "***********************"
-  secret_key = "***********************"
+  access_key = "AKIAZXOHZTTX5VLY7YLE"
+  secret_key = "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"
 }
